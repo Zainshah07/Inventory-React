92d0628fbc5b5bf1e9845e4263bea8a241d4d292
Zainshah07 <s.zainshah725@gmail.com>
2025-12-06 22:54:37 +0500

    deployment path

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Brand from "./pages/Brand";
 import Item from "./pages/Item";
 import Model from "./pages/Model";
 import { ToastContainer, toast } from "react-toastify";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/items" />} />
           <Route path="/brands" element={<Brand />} />
           <Route path="/items" element={<Item />} />
           <Route path="/models" element={<Model />} />
